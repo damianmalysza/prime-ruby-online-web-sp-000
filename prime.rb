2e1987a20aa75abf92b577b6cf7022eb5ce291ec
Damian Malysza <damian.malysza@gmail.com>
2020-10-20 21:37:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,7 @@
 def prime?(int)
   return false if int <= 1
   return true if int == 2
-  if (2..int-1).to_a.any? do |i|
-    int % i == 0
-  end
+  if (2..int-1).to_a.any? {|i| int % i == 0 }
     return false
   else
     return true
